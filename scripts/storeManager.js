ac65d4b4a7d449e9b1c2da1582095a194d99b2f8
timothythomas1 <tuxes-parlay0s@icloud.com>
2022-07-13 19:44:21 -1000

    Assignment 2 so far

---
+++ scripts/storeManager.js
@@ -0,0 +1,19 @@
+const KEY = "users";
+function saveUser(userObj){
+    let oldData = readUsers(); // Getting the old data thats on local storage at the moment
+    oldData.push(userObj); //add the new data to the local storage
+    let value = JSON.stringify(userObj); // 'stringify' converts objects to string
+    // send the user to the local database
+    localStorage.setItem(KEY, value);
+}
+function readUsers(){
+    let data = localStorage.getItem(KEY);
+    console.log(data); // <-JSON
+    if(!data){//is not data
+        return []; //initializing an empty array when the is no data
+    }
+    else{// we now have data
+        let list = JSON.parse(data); // 'parse' is from a string into an object
+        return list;
+    }
+}
